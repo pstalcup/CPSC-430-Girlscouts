0ec1b8c54e62e0e3f2d4a7f7c454bb23d6781a8d
Shane Lawton <slawton@rosemary.umw.edu>
2012-03-19 15:56:49 -0400

    second batch of skeleton files.

---
+++ manager.php
@@ -0,0 +1,11 @@
+<html>
+
+<!---
+A link to this page should become visible on the menu bar if the logged in user is the administrator.
+allows administrator to edit upcoming events, news, photos, and incentives.
+
+should display a news entry box and links to the events, photos, and inventory manager pages.
+---->
+
+
+</html>
---
+++ ninventory.php
@@ -0,0 +1,8 @@
+<html>
+
+<!---
+I'm not sure exactly what this is supposed to be. It's the Nut Sales Troop Worksheet, which doesn't have a cookie counterpart. I assumed it was some form of inventory tracker, but that could be wrong.
+---->
+
+
+</html>
