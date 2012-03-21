fa632d20b57bef0b333c79035b5f697632cf506d
= <imartin2@mail.umw.edu>
2012-03-21 09:54:30 -0400

    Add girls to database

---
+++ addGirlsController.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+ob_start();
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Add Girl</title>
+  
+</head>
+
+<body>
+<div id="contents">
+
+
+
+
+<div>
+
+
+
+
+</body>
+</html>
