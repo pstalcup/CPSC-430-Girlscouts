2353a6989259a8f2f79889a00dca12d34a30c607
= <imartin2@mail.umw.edu>
2012-03-21 09:50:50 -0400

    Add girls to Database

---
+++ addGirls.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+?>
+<HTML>
+
+<font color = "green" face = "century gothic"><h1>ADD GIRL TO TROOP</h1></font>
+
+
+<font face = "century gothic">
+
+<form action =" " method = "post">
+<table border="1">
+<text align="left">
+<tr>
+<td> First Name <input type = "text" name = "firstname" style="width:200px;" />
+Last Name <input type = "textarea" name = "lastname" style="width:200px" /></td>
+</tr>
+<tr>
+<td>Address <input type = "textarea" name = "address" style="width:492px" /></td>
+</tr>
+<tr>
+<td>City <input type = "textarea" name = "city" style = "width:125px" />
+State <input type = "textarea" name = "state" style = "width:35px" />
+Zip Code <input type = "textarea" name = "zipcode" style = "width:75px" />
+DOB <input type ="textarea" name = "DOB" style = "width: 137px" />
+</tr>
+</text>
+</table>
+</form>
+
+</font>
+</br>
+
+<input type="submit" value="Add More Girls" />
+<input type="submit" value="Done" />
+
+
+</body>
+</HTML>
