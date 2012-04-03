ccc3608f7682b2ab93ffd1dc217e007ea8cffb67
slawton <slawton@rosemary.umw.edu>
2012-04-03 17:02:03 -0300

    cool cool cool

--- contactController.php
+++ contactController.php
@@ -1,6 +1,27 @@
 <html>
+HI
+<?php
+  session_start();
+  include "db_connect.php";
+  include "loggedIn.php";
+?>
+temporarily coding in this page until I can push the appropriate file at home.
 
-<!--- reads POST data and submits new contact information to database  ---->
+<?php
+  if(isset($_POST['game'])) {
+    $title = mysqli_real_escape_string($db, trim($_POST['game']));
+  } else { $title = ""; }
+
+/*
+
+
+
+
+*/
+
+?>
+
+
 
 
 </html>
