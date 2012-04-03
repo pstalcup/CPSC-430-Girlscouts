3818352dc1a5f5e5b5088db37f00d53f073ea444
= <imartin2@mail.umw.edu>
2012-04-02 22:06:35 -0400

    fixed the database! It should be error free at this point.

--- girlScouts.sql
+++ girlScouts.sql
@@ -1,50 +1,29 @@
-/*
-
-Girls Scouts database
-
-Tables:
-cookies
-nuts
-girls
-users
-requests
-events
-
-*/
-
 DROP DATABASE IF EXISTS `S12-cpsc430G3`;
 CREATE DATABASE IF NOT EXISTS `S12-cpsc430G3`;
 USE `S12-cpsc430G3`;
 
 CREATE TABLE IF NOT EXISTS `products` (
-  `productId` INT(5) AUTO_INCREMENT NOT NULL,
+  `productId` INT(5) AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `types` ENUM('Cookie', 'Nut') NOT NULL,
   `name` varchar(40) NOT NULL,
   `price` float(5) NOT NULL,
   `description` varchar(70) NOT NULL,
-  `quantity` int(5) NOT NULL,
-  PRIMARY KEY (`productId`)
-) ;
+  `quantity` int(5) NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS `transactions`(
-  TID INT(10) auto_increment NOT NULL,
-  girlId INT(5) NOT NULL,
-
-FOREIGN KEY girlId REFERENCES girls(id)
+  TID INT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  girlId INT(5) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `sales`(
-  `transactionId` INT(10) AUTO_INCREMENT,
+  `transactionId` INT(10) NOT NULL REFERENCES transactions(TID),
   `quantity` INT(5) DEFAULT 0 NOT NULL,
-  `productId` INT(3) NOT NULL
-
-FOREIGN KEY(productId) REFERENCES products(productId)
-
+  `productId` INT(3) NOT NULL REFERENCES products(productId)  
 );
 
-
 CREATE TABLE IF NOT EXISTS `girls`(
-  `id` INT(3) AUTO_INCREMENT PRIMARY KEY,
+  `girlId` INT(3) AUTO_INCREMENT PRIMARY KEY,
   `firstName` varchar(30) NOT NULL,
   `lastName` varchar(30) NOT NULL,
   `DOB` varchar(9) NOT NULL,
@@ -53,9 +32,7 @@ CREATE TABLE IF NOT EXISTS `girls`(
   `st` varchar(2) NOT NULL,
   `zip` int(5) NOT NULL,
   `sales` INT(5) DEFAULT 0
-  
 ) ;
-
 
 CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(40) NOT NULL PRIMARY KEY,
@@ -71,30 +48,19 @@ CREATE TABLE IF NOT EXISTS `users` (
   `licenseNum` varchar(12),
   `ssNum` varchar(10),
   `girlId` int(3) DEFAULT 0
- 
 );
 
 CREATE TABLE IF NOT EXISTS `requests`(
 `email` varchar(50) NOT NULL,
 `daughter` varchar(70) NOT NULL,
 
-FOREIGN KEY(email) REFERENCES users(email)
-
-);
-
-CREATE TABLE IF NOT EXISTS `demographics`(
-`parent` varchar(40) NOT NULL,
-`race` ENUM('American Indian or Alaskan Native', 'Asian', 'Black or African American', 'Hawaiian or Pacific Islander', 'White', 'Other'),
-`hispanicOrLatino` BOOLEAN NOT NULL ,
-`gender` ENUM('Female', 'Male'),
-`numYearsScouting` INT(2) NOT NULL 
-
-FOREIGN KEY(`parent`) REFERENCES `users`(`email`)
+FOREIGN KEY(email) 
+REFERENCES users(email)
 );
 
 CREATE TABLE IF NOT EXISTS `events`(
 `eventId` int(5) AUTO_INCREMENT PRIMARY KEY,
-`dateOfEvent` date NOT NULL,
+`dateOfEvent` DATE NOT NULL,
 `timeOfEvent` TIME NOT NULL,
 `name` varchar(50) NOT NULL,
 `description` blob,
@@ -103,13 +69,11 @@ CREATE TABLE IF NOT EXISTS `events`(
 
 CREATE TABLE IF NOT EXISTS `attending`(
 `eventId` int(5) NOT NULL,
-`girlId` int(5) NOT NULL
+`girlId` int(5) NOT NULL,
 
-FOREIGN KEY(`eventId`) REFERENCES `events`(`eventId`)
+FOREIGN KEY(eventId) REFERENCES events(eventId)
 
 );
-
-
 
 
 SHOW TABLES;
