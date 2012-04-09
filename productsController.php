f715c6bdfd754dce84c7b8163d285e198f3195c2
Shane Lawton <rosemary.umw.edu>
2012-04-09 15:30:03 -0400

    more work on insert products

--- productsController.php
+++ productsController.php
@@ -1,36 +1,27 @@
 <?php
 	session_start();
-	include "loggedIn.php";
+	//include "loggedIn.php";
 	include "db_connect.php";
 	
 	$rows = 1;
 	while(isset($_POST["name".$rows])) {
-		echo "ok<br/>";
-		
+		if($_POST["name".$rows] != "") {
+			$name = $_POST["name".$rows];
+			$price = $_POST["price".$rows];
+			$type = $_POST["type".$rows];
+			$desc = $_POST["desc".$rows];
+			$quantity = 10;
+			
+			//$query = "update products set productid=null, types'$type','$name',$price,'$desc',$quantity);";
+			$query = "replace into products ('types','name','price','description','quantity') values ('$type','$name',$price,'$desc',$quantity);";
+			$result = mysqli_query($db, $query) or die ("ERROR INSERTING");
+
+			echo $query."<br/>";
+		}
 		$rows++;
 	}
-/*
-	if(isset($_POST['type'])) {
-		$type = mysqli_real_escape_string($db, trim($_POST['type']));
-	} else { $type = ""; }
-	if(isset($_POST['name'])) {
-		$name = mysqli_real_escape_string($db, trim($_POST['name']));
-	} else { $name = ""; }
-	if(isset($_POST['price'])) {
-	  	$price = mysqli_real_escape_string($db, trim($_POST['price']));
-	} else { $price = ""; }
-	if(isset($_POST['desc'])) {
-	  	$desc = mysqli_real_escape_string($db, trim($_POST['desc']));
-	} else { $desc = ""; }
-	if(isset($_POST['quantity'])) {
-	  	$quantity = mysqli_real_escape_string($db, trim($_POST['quantity']));
-	} else { $quantity = ""; }
-
-	$query = "insert into table products ('types','name','price','description','quantity') values ($type,$name,$price,$desc,$quantity);";
-	$result = mysqli_query($db, $query) or die ("ERROR INSERTING");
-
-
-	header("Location: products.php");
+	
+	echo "<br/><a href=\"products.php\">Continue</a>";
+	//header("Location: products.php");
 	exit("");
-*/
 ?>
