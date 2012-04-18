6c887bbe7ccab9d4a2e0bd12cadc2623ec632270
= <imartin2@mail.umw.edu>
2012-04-17 23:01:22 -0400

    update

--- addGirlsController.php
+++ addGirlsController.php
@@ -3,13 +3,10 @@ include "db_connect.php";
 $needsadmin = true;
 include "loggedIn.php";
 
-	$firstName = trim($_GET['firstName']);
-	$lastName = trim($_GET['lastName']);
-	$DOB = trim($_GET['DOB']);
-	$address = trim($_GET['address']);
-	$city = trim($_GET['city']);
-	$st = $_GET['st']);
-	$zip = trim($_GET['zip']);
+	foreach($_GET as $key => $value)
+	{
+		$$key = $value;
+	}
   
 	$query = "INSERT INTO events (dateOfEvent,timeOfEvent,name,description,location) VALUES ('$date','$time','$name','$description','$location');";
 	echo $query;
