3bbd24dae6ce9ef5e72f88115536732db7ec3a37
= <imartin2@mail.umw.edu>
2012-04-18 11:18:22 -0400

    update

--- boothSales.php
+++ boothSales.php
@@ -11,6 +11,13 @@
 <table>
 <tr>
 	<td>Date </td>
+	<td>
+			<input type="text" name="day" size=1 maxlength=2>/
+			<input type="text" name="month" size=1 maxlength=2>/
+			<input type="text" name="year" size=1 maxlength=2> (dd/mm/yy)
+
+	</td>
+	
 	
 
 
