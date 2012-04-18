b37bcbbc4f3c9d0523d834f1dc0e139eb01a2379
= <imartin2@mail.umw.edu>
2012-04-17 22:51:15 -0400

    update

--- addGirlsController.php
+++ addGirlsController.php
@@ -2,33 +2,18 @@
 include "db_connect.php";
 $needsadmin = true;
 include "loggedIn.php";
-?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Add Girl</title>
+
+	$firstName = trim($_GET['firstName']);
+	$lastName = trim($_GET['lastName']);
+	$DOB = trim($_GET['DOB']);
+	$address = trim($_GET['address']);
+	$city = trim($_GET['city']);
+	$st = $_GET['st']);
+	$zip = trim($_GET['zip']);
   
-</head>
-
-<body>
-
-<?php
-
-
-	$firstName = mysqli_real_escape_string($db, trim($_GET['firstName']));
-	$lastName = mysqli_real_escape_string($db, trim($_GET['lastName']));
-	$DOB = mysqli_real_escape_string($db, trim($_GET['DOB']));
-	$address = mysqli_real_escape_string($db, trim($_GET['address']));
-	$city = mysqli_real_escape_string($db, trim($_GET['city']));
-	$st = mysqli_real_escape_string($db, trim($_GET['st']));
-	$zip = mysqli_real_escape_string($db, trim($_GET['zip']));
-  
-	$query = "INSERT INTO girls (firstName, lastName, DOB, address, city, st, zip) VALUES ($firstName, $lastName, $DOB, $address, $city, $st, $zip);"
-  
+	$query = "INSERT INTO events (dateOfEvent,timeOfEvent,name,description,location) VALUES ('$date','$time','$name','$description','$location');";
+	echo $query;
+	mysqli_query($db,$query);
+	Header("Location: main.php");
 
 ?>
-
-
-
-</body>
-</html>
