8cf3a2ef88ffb20bf50f845ba31abe48b77b1e63
= <imartin2@mail.umw.edu>
2012-04-18 01:26:27 -0400

    update

--- registerSale.php
+++ registerSale.php
@@ -5,9 +5,19 @@ include "loggedIn.php";
 $types = array (
 'Cookie', 'Nut' );
 
-function types(
+#generate dropdown list of product type
+function typesMenu($name = ' ', $options = array()){
+	$html = '<select name="'.$name.'">';
+	foreach ($options as $option => $value) {
+		$html .= '<option value='.$value.'>'.$option.'</option>';
+	}
+	$html .= '</select>';
+	return $html;
+}
 
 
+
+$type = typesMenu('types', $types);
 
 
 ?>
@@ -21,6 +31,7 @@ function types(
 
 	
 <form method="post" action="registerSaleController.php">
+
  <table border="1" cellpadding="5" cellspacing="5">
  <tr><th>Customer</th><th>Type</th><th>Product</th><th>Quantity</th></tr>
  <tr><td><input type = 'text' /></td><td><?php echo $type;?></td><td><?php echo $product;?></td><td><input type='text' name='qty'/> </td><tr>
