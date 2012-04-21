06055f39a2cbe6f95a2e6ecaf246de228418f303
Ivania Martinez <imartin2@mail.umw.edu>
2012-04-21 15:39:33 -0400

    update

--- salesReport.php
+++ salesReport.php
@@ -22,7 +22,7 @@ while($row = mysqli_fetch_array($result)) {
 	$name = $row['name'];
 	$quantity = $row['quantity'];
 	$price = $row['price'];
-	$total = $row['(s.quantity * p.price)'];
+	$total = $row['($quantity * $price)'];
 		
 echo "<tr><td>$name  </td><td>$quantity </td><td>$price </td><td> $total</td></tr>\n";
 }
