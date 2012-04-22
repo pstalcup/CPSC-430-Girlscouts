25e617855e0589c53bb8f7558327d6ffc896c913
Shane Lawton <rosemary.umw.edu>
2012-04-22 19:45:59 -0400

    removed old forms

--- forms/ninventory.php
+++
@@ -1,8 +0,0 @@
-<html>
-
-<!---
-I'm not sure exactly what this is supposed to be. It's the Nut Sales Troop Worksheet, which doesn't have a cookie counterpart. I assumed it was some form of inventory tracker, but that could be wrong.
----->
-
-
-</html>
