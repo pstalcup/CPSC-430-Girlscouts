8070fa7a0d37ebd6d417e84b01b1c503e2bbff1e
Patrick Stalcup <legomaniac0117@gmail.com>
2012-04-22 18:57:59 -0400

    asdf

---
+++ mainController.php
@@ -0,0 +1,15 @@
+<?php
+	include "db_connect.php";
+	include "menu.php";
+	
+	$query = "DELETE FROM attending WHERE girlId = (SELECT girlId FROM users WHERE email='$e');";
+	echo $query."</br>";
+	mysqli_query($db,$query);
+	foreach($_GET as $key => $value)
+	{
+		$query = "INSERT INTO attending (eventId, girlId) VALUES ('$key',(SELECT girlId FROM users WHERE email='$e'));";
+		echo $query;	
+		mysqli_query($db,$query);
+	}
+	Header("Location: main.php");
+?>
