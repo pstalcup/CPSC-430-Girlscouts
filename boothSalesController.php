dd0cae346f98ef078561d02ce7e565908e3ce490
= <imartin2@mail.umw.edu>
2012-04-22 23:27:16 -0400

    slowly fixing these errors

--- boothSalesController.php
+++ boothSalesController.php
@@ -1,16 +1,5 @@
 <?php
-	
-include "db_connect.php";
-$requires = "admin";
-include "menu.php";	
-
-
-
 	$total = 0;
-	
-	
-
-
 
 	include "db_connect.php";
 	$requires = "admin";
@@ -21,14 +10,11 @@ include "menu.php";
 	}
 	$date = "20".$year."-".$month."-".$day;
 	$time = time();
-<<<<<<< HEAD
+
 	$name = "Booth Sale";
 	$description = "Booth Sale";
 	$location = $location;
-=======
-	$location = $location;
 
->>>>>>> 260f2328945d9739274ea623f300858eeeadf55a
 	//enter booth sale into event based on date
 	$query ="INTO events (dateOfEvent,timeOfEvent,name,description,location) VALUES ('$date','$time','$name','$description','$location');";
 
