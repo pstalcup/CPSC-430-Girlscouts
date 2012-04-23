7c828ebb51d6e221642d0d9c5e5d609a68bfb3e9
= <imartin2@mail.umw.edu>
2012-04-23 02:20:10 -0400

    making sure booth sales don't show up as events an user an attend, since the admin will be the only one to have access to booth sales.

--- viewEvent.php
+++ viewEvent.php
@@ -14,7 +14,7 @@
 
 	echo "<table cellpadding=\"5\"";
 	echo '<tr><th>Event</th><th>Date</th><th>Time</th><th>Attending</th></tr>';
-	$query = "SELECT name,description,DATE_FORMAT(dateOfEvent,'%m/%d') as dateOfEvent,TIME_FORMAT(timeOfEvent,'%l:%i') as timeOfEvent,eventId FROM events WHERE name <> LIKE'Booth Sal%' AND DATEDIFF(dateOfEvent,CURRENT_DATE()) > 0;";
+	$query = "SELECT name,description,DATE_FORMAT(dateOfEvent,'%m/%d') as dateOfEvent,TIME_FORMAT(timeOfEvent,'%l:%i') as timeOfEvent,eventId FROM events WHERE name <> 'Booth Sale' AND DATEDIFF(dateOfEvent,CURRENT_DATE()) > 0;";
 	$events = mysqli_query($db,$query);
 	while($row = mysqli_fetch_array($events)) {
 		echo "<tr><td>".$row["name"]."</td>";
