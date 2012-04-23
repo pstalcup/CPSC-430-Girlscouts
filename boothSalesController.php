d660fc35327aff0f0239cdc3404af6869890fa07
= <imartin2@mail.umw.edu>
2012-04-23 03:07:27 -0400

    working on changes

--- boothSalesController.php
+++ boothSalesController.php
@@ -21,17 +21,36 @@
 	//enter booth sale into event based on date to help admin keep track of location and date of booth sale
 	$query ="INSERT INTO events (dateOfEvent,timeOfEvent,name,description,location) VALUES ('$date','$time','$name','$description','$location');";
 	mysqli_query($db,$query);
-	Header("Location: main.php");
+	
 
 	//Add sales
 	$total = 0;
 	$attendees = 0;
+	
 	while(isset($_POST['pname'])){
 	$product = $_POST['pname'];
 	
-	$query = "select productId from products where productId = 
+	$query = "select productId from products where name = '$product';";
+	$pid = mysqli_query($db,$query);
+	
+	$qty = $_POST[$qtyid];
+	
+	$query = "select quantity from products where name = '$product';";
+	$currentQty = mysqli_query($db,$query);
+	//can't substract from quantity if not available in inventory
+	if($currentQty > $qty){
+		$newQty = $currentQty - $qty;
+		$query = "UPDATE products SET quantity = '$newQty';";
+		mysqli_query($db,$query);
+	}else {
+		echo "The quantity available isn't enough.";
+	}
+	
+	$total + = $qty;
+	$attendees += 1;
 	
 	}
 	
+	Header("Location: main.php");
 
 ?>
