fe6dadee603430ecd94817f42ef8df02f9666303
= <imartin2@mail.umw.edu>
2012-04-22 23:20:47 -0400

    update

--- boothSalesController.php
+++ boothSalesController.php
@@ -1,6 +1,15 @@
 <?php
 
-
-
+	include "db_connect.php";
+	$requires = "admin";
+	include "menu.php";
+	foreach($_GET as $key => $value)
+	{
+		$$key = $value;
+	}
+	$date = "20".$year."-".$month."-".$day;
+	$time = time();
+	//enter booth sale into event based on date
+	$query ="INTO events (dateOfEvent,timeOfEvent,name,description,location) VALUES ('$date','$time','$name','$description','$location');";
 
 ?>
