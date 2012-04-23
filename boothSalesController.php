58c74d47d7fae9416df94f01ae6f91c3a1b16ea6
= <imartin2@mail.umw.edu>
2012-04-22 23:25:32 -0400

    update

--- boothSalesController.php
+++ boothSalesController.php
@@ -9,6 +9,9 @@
 	}
 	$date = "20".$year."-".$month."-".$day;
 	$time = time();
+	$name = "Booth Sale";
+	$description = "Booth Sale";
+	$location = $location;
 	//enter booth sale into event based on date
 	$query ="INTO events (dateOfEvent,timeOfEvent,name,description,location) VALUES ('$date','$time','$name','$description','$location');";
 
